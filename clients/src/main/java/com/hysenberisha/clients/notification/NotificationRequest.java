9178f780b4fbd8baf4dd84aed4c0b13fb87cc99e
HysenB <hysi2000@live.com>
2022-06-02 02:52:06 +0200

    adding notification-service

---
+++ clients/src/main/java/com/hysenberisha/clients/notification/NotificationRequest.java
@@ -0,0 +1,8 @@
+package com.hysenberisha.clients.notification;
+
+public record NotificationRequest(
+        Integer toCustomerId,
+        String toCustomerName,
+        String message
+) {
+}
--- customer/src/main/java/com/hysenberisha/customer/CustomerService.java
+++ customer/src/main/java/com/hysenberisha/customer/CustomerService.java
@@ -2,6 +2,8 @@ package com.hysenberisha.customer;
 
 import com.hysenberisha.clients.fraud.FraudCheckResponse;
 import com.hysenberisha.clients.fraud.FraudClient;
+import com.hysenberisha.clients.notification.NotificationClient;
+import com.hysenberisha.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,8 +13,9 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+//    private final RestTemplate restTemplate;
     private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -37,6 +40,15 @@ public class CustomerService {
         if(fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("Fraudster");
         }
-        //  todo: send notification
+        //  todo: make it asnyc i.e add to queue
+        notificationClient.sendNotification(
+                new NotificationRequest(
+                        customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi %s, welcome to Hysenberisha...",
+                                customer.getFirstName())
+                )
+        );
+
     }
 }
